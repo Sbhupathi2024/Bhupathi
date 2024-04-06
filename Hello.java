5d5f68de00e4f67572ca342a79e66cf0406928ec
Sbhupathi2024 <166191857+Sbhupathi2024@users.noreply.github.com>
2024-04-05 23:31:53 -0500

    Update Hello.java
    Commit with System.out.println statment.

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello pokkuluri Chakri");
+        System.out.println("Hello World");
+        System.out.println("Hello Bhupathi Roja");
     }
 }
